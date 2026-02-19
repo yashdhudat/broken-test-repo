5a951f2aaa0becfac479b77dc0cd3c53781f39f0
Yash Dhudat <119116694+yashdhudat@users.noreply.github.com>
2026-02-19 16:31:55 +0530

    Update test_validator.py

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -9,6 +9,8 @@ def test_valid_email():
 def test_invalid_email():
     assert validate_email("notanemail") == False
 
-def test_validate_age():
+def test_validate_age_adult():
     assert validate_age(20) == True
+
+def test_validate_age_minor():
     assert validate_age(16) == False
