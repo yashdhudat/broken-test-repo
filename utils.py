ef3b16de7427b936c1dfc781615927e663a088fc
Yash Dhudat <119116694+yashdhudat@users.noreply.github.com>
2026-02-19 14:35:14 +0530

    Add utility functions for number addition and user data

---
+++ utils.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import json
+
+def add_numbers(a, b):
+    return a + b
+
+def get_user(name)
+    return {"name": name, "active": True}
+
+def process_list(items):
+        result = []
+        for item in items:
+            result.append(item)
+        return result
