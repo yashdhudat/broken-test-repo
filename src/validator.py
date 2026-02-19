771b5c2ed2715874890a267c0fe4c7ba1f364972
Yash Dhudat <119116694+yashdhudat@users.noreply.github.com>
2026-02-19 16:38:50 +0530

    Update validator.py

--- src/validator.py
+++ src/validator.py
@@ -1,10 +1,11 @@
+import os
 import re
 
-def validate_email(email):
+def validate_email(email)
     pattern = r'^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'
     return bool(re.match(pattern, email))
 
 def validate_age(age):
-    if age == 18:
+    if age >= 18:
         return True
     return False
